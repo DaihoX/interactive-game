e6b32885eeff80911046a5225751296f5538b9cc
zBugStar <juan.orozco45208@ucaldas.edu.co>
2024-10-03 10:34:36 -0500

    Add delete files

---
+++ src/iniciar_partida.py
@@ -0,0 +1,26 @@
+class Habitacion:
+    def __init__(self, nombre, descripcion, acertijo):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.acertijo = acertijo
+
+class Acertijo:
+    def __init__(self, pregunta, respuesta):
+        self.pregunta = pregunta
+        self.respuesta = respuesta
+        
+print("\nTe despiertas de la nada, te duelen los ojos y todo esta oscuro. No recuerdas como llegaste aqui \n")
+print("\nSolo puedes ver 3 luces enfrente tuyo, ¿que deseas hacer? \n")
+Class iniciar_partida:
+    
+eleccion = input("parece que debes elegir una de las piedras, ¿que piedra eliges, Azul, Roja, Amarillo?")
+if eleccion == "azul" and "Azul":
+    print("una puerta se abre al frente tuyo todo se torna con un color azulado\n")
+    print("una puerta se abre al frente tuyo todo se torna con un color azulado\n")
+    acertijo1 = Acertijo("¿Cuál es el número más pequeño primo?", "2")
+    habitacion1 = Habitacion("Sala Oscura", "Una sala llena de sombras y misterio.", acertijo1)
+else: eleccion == "rojo" and "Rojo"
+print("una puerta se abre al frente tuyo todo se torna con un color rojiso\n")
+print("una puerta se abre al frente tuyo todo se torna con un color rojiso\n")
+
+# Ejemplo de habitaciones y acertijos
---
+++ src/main.py
@@ -0,0 +1,51 @@
+import time
+import sys
+import iniciar_partida
+def mostrar_menu():
+    print("\n--- Bienvenido a Corona de Hielo---")
+    print("Corona de Hielo es una aventura interactiva de puzzles")
+    print("1. Iniciar una nueva partida")
+    print("2. Cargar partida")
+    print("3. Historia")
+    print("4. Salir")
+
+
+def escribir_poco_a_poco(texto = "hola como estas", retraso=0.1):
+    for letra in texto:
+        sys.stdout.write(letra)
+        sys.stdout.flush()  # Asegura que se imprima el carácter inmediatamente
+        time.sleep(retraso)  # Tiempo de retraso entre cada carácter
+    print()  # Añade un salto de línea al final    
+    
+class main():
+
+    while True:
+        mostrar_menu()
+        opcion = input("Elija una opción: ")
+
+        if opcion == "1":
+            #iniciar partida
+            iniciar_partida()
+        elif opcion == "2":
+            #Cargar una partida de los 3 slots de guardado
+            opcion2 = input("Seleccione se partida")
+            if opcion2 == "1":
+                print("Cargando partida")
+            elif opcion2 == "2":
+                print("Cargando partida")
+            elif opcion2 == "3":
+                print("Cargando partida")
+        elif opcion == "3":
+            #Por terminar, idea generar una cadena texto que se imprima poco a poco con al rededor de 0.1 segundos entre caracter para dar un estilo visual mas limpio 
+            #Iniciar historia y volver al menu
+            escribir_poco_a_poco()
+
+        elif opcion == "4":
+            #Terminar ejecucion de el juego
+            print("Gracias por jugar")
+            sys. exit()
+
+        else:
+            print("Opción no válida. Inténtelo de nuevo.")
+
+mostrar_menu()
